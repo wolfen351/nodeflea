c86434fa27a627a18c92d2f3a733f054c8c4a404
wolfen351 <projects@wolfen.za.net>
2017-11-26 17:07:44 +0400

    fixed typo

--- plugins/ping.js
+++ plugins/ping.js
@@ -5,7 +5,7 @@ function calcResponse(message, dest, source)
   return "PING " + message + " for " + dest;
 }
 
-exports.commandName = 'ping;;
+exports.commandName = 'ping';
 exports.responseFunction = calcResponse;
 exports.helpText = 'dns/ip [count] - Ping the IP or DNS entry, count is optional (up to 4)';
 
