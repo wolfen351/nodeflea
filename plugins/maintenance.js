3da9e71e12bbd241cb35ff259489833969d10d3f
wolfen351 <projects@wolfen.za.net>
2017-12-01 02:41:46 +1300

    added maintenance command

---
+++ plugins/maintenance.js
@@ -0,0 +1,27 @@
+/* maintenance module for flea */
+
+
+function ChatModule () {
+	
+    /* THIS METHOD WILL BE PROVIDED BY THE HOST - CALL IT TO SEND MESSAGES */
+	/*this.sendMessage = function(dest, message)
+	{
+		console.log(dest, " <- ", message);
+	}*/
+	
+
+	this.messageReceived = function(message, dest, source)
+	{
+		var words = message.substr(message.indexOf(" ") + 1);	
+		
+		this.sendMessage(dest, "Shutting down for maintenance: " + words);
+	}
+
+}
+
+exports.commandName = 'maintenance';
+exports.module = new ChatModule();
+exports.helpText = 'message - Shuts down flea for maintenance';
+
+
+
